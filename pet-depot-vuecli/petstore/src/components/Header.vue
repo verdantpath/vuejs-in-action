4329776d58e984b2c91c8209949825f61a1e5dab
verdantpath <m_dittmann@yahoo.com>
2020-07-12 21:25:00 -0700

    add Header component

---
+++ pet-depot-vuecli/petstore/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+<header>
+  <div class="navbar navbar-default">
+    <div class="navbar-header">
+      <h1><router-link :to="{name: 'iMain'}">{{ sitename }}</router-link></h1>
+    </div>
+    <div class="nav navbar-nav navbar-right cart">
+        <router-link active-class="active" tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
+        <span class="glyphicon glyphicon-shopping-cart">{{ cartItemCount}}</span> Checkout
+        </router-link>
+      </button>
+    </div>
+  </div>
+</header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-header',
+  data() {
+    return {
+      sitename: "Vue.js Pet Depot",
+    }
+  },
+  props: ['cartItemCount'],
+  methods: {
+    showCheckout() {
+      this.$router.push({ name: 'Form' });
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
