3a85be9ff27ecbc7150106b1b037f90569830fc1
verdantpath <m_dittmann@yahoo.com>
2020-08-13 19:27:56 -0700

    Initial commit

---
+++ jsx/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  render(h) {
+    return (
+      <div>
+        <div class="text-center"
+        on-click={this.pressed}
+        domPropsInnerHTML={this.msg}></div>
+      </div>
+    )
+  },
+  data () {
+    return {
+      welcome: 'Hello World ',
+      msg: `<h${this.header}>Hi, my name is ${this.name}</h${this.header}>`,
+    }
+  },
+  methods: {
+    pressed() {
+      alert('Clicked')
+    }
+  },
+  props:['header', 'name']
+}
+</script>
+
+<style scoped>
+</style>
