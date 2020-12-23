541396c7666c62479e776113354acebeb10afe42
verdantpath <m_dittmann@yahoo.com>
2020-12-22 21:22:30 -0800

    Add starting files from petstore project, Add vuefire

---
+++ pet-depot-nuxt/petstore/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s ease-out;
+}
+.fade-enter,
+.face-leave-to {
+  opacity: 0.5;
+}
+</style>
