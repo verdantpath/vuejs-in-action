93cd44289121fe842e6dd71bea308b41aa737866
verdantpath <m_dittmann@yahoo.com>
2022-09-20 18:47:11 -0700

    Add Header file

--- pet-depot-nuxt/petstore/src/components/Header.vue
+++ pet-depot-nuxt/petstore/src/components/Header.vue
@@ -7,6 +7,17 @@
       <h1><router-link :to="{name: 'iMain'}">{{ sitename }}</router-link></h1>
     </div>
     <div class="nav navbar-nav navbar-right cart">
+      <div v-if="!mySession">
+        <button type="button" class="btn btn-default btn-lg" v-on:click="signIn">
+          Sign In
+        </button>
+      </div>
+      <div v-else>
+        <button type="button" class="btn btn-default btn-lg" v-on:click="signOut">
+          <img class="photo" :src="mySession.photoURL" />
+          Sign Out
+        </button>
+      </div>
         <router-link active-class="active" tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
         <span class="glyphicon glyphicon-shopping-cart">{{ cartItemCount}}</span> Checkout
         </router-link>
@@ -17,16 +28,46 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 export default {
-  name: 'my-header',
+  name: 'Header',
   data() {
     return {
-      sitename: "Vue.js Pet Depot",
+      sitename: "Vue.js Pet Depot"
     }
   },
   props: ['cartItemCount'],
+  beforeCreate() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.$store.commit('SET_SESSION', user || false)
+    });
+  },
   methods: {
-    
+    showCheckout() {
+      this.$router.push({name: 'Form'});
+    },
+    signIn() {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log('signed in');
+      }.catch(function(error) {
+        console.log('error ' + error)
+      });
+    },
+    signOut() {
+      firebase.auth().signOut().then(function() {
+        // Sign-our succesful
+        console.log('signed out!')
+      }).catch(function(error) {
+        console.log('error in sign out!')
+        // An error happened
+      });
+    }
+  },
+  computed: {
+    mySession() {
+      return this.$store.getters.session;
+    }
   }
 }
 </script>
@@ -35,6 +76,10 @@ export default {
 a {
   text-decoration: none;
   color: black;
+}
+.photo {
+  width: 25px;
+  height: 25px;
 }
 .router-link-exact-active {
   color: #000;
